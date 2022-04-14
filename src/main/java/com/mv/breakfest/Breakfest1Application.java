c469cb0c26e724e6bb4f63281e24998bbdd728f0
pedrovbo <pedrovicboliveira@gmail.com>
2022-04-14 12:07:31 -0300

    configuring heroku

--- src/main/java/com/mv/breakfest/Breakfest1Application.java
+++ src/main/java/com/mv/breakfest/Breakfest1Application.java
@@ -2,10 +2,20 @@ package com.mv.breakfest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class Breakfest1Application {
 
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Breakfest1Application.class, args);
 	}
