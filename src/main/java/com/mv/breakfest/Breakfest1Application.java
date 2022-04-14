8851a98534ebafbc96dee3f7a65acabdca07c5ef
pedrovbo <pedrovicboliveira@gmail.com>
2022-04-14 12:14:01 -0300

    configuring to deploy

--- src/main/java/com/mv/breakfest/Breakfest1Application.java
+++ src/main/java/com/mv/breakfest/Breakfest1Application.java
@@ -2,19 +2,21 @@ package com.mv.breakfest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
-@Controller
+@RestController
+@RequestMapping("/")
 public class Breakfest1Application {
 
-	@RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
+	
+	@GetMapping
+	public ModelAndView swaggerUi() {
+	return new ModelAndView("redirect:/swagger-ui/");
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Breakfest1Application.class, args);
