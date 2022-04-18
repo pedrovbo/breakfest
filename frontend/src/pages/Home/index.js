5e4df57e057cee7572e5e907c6b45875065549b6
pedrovbo <pedrovicboliveira@gmail.com>
2022-04-18 16:45:45 -0300

    add colaborador component

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,6 +1,30 @@
+import React from "react";
+import { Container, ColaboradorRow } from "./styles";
+
 function Home() {
+  const colaboradores = [
+    {
+      id: 1,
+      nome: "João",
+      cpf: "123456789",
+      opcaoCafeManha: "Café",
+    },
+    {
+      id: 2,
+      nome: "Gordon",
+      cpf: "653452313",
+      opcaoCafeManha: "Cuscuz",
+    },
+    {
+      id: 3,
+      nome: "Paula",
+      cpf: "5434222",
+      opcaoCafeManha: "Bolo",
+    }
+  ];
+
   return (
-    <div>
+    <Container>
       <h1>Breakfest</h1>
       <h3>Lista de Colaboradores</h3>
       <div id="table-container">
@@ -14,22 +38,20 @@ function Home() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Pedro</td>
-              <td>213432453</td>
-              <td>Café</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Angela</td>
-              <td>31213131</td>
-              <td>Chá</td>
-            </tr>
+            {colaboradores.map((colaborador) => {
+              return (
+                <ColaboradorRow key={colaborador.id}>
+                  <td>{colaborador.id}</td>
+                  <td>{colaborador.nome}</td>
+                  <td>{colaborador.cpf}</td>
+                  <td>{colaborador.opcaoCafeManha}</td>
+                </ColaboradorRow>
+              );
+            })}
           </tbody>
         </table>
       </div>
-    </div>
+    </Container>
   );
 }
 
---
+++ frontend/src/pages/Home/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  h1 {
+    text-align: center;
+  }
+
+  h3 {
+    text-align: left;
+    padding: 3% 10% 1% 10%;
+  }
+
+  table {
+    text-align: center;
+    transition: 3s;
+  }
+
+  #table-container {
+    padding: 0% 10% 0% 10%;
+  }
+
+  .table-titles {
+    font-weight: bold;
+    font-size: larger;
+  }
+`
+
+export const ColaboradorRow = styled.tr`
+  transition: all 0.3s;
+
+  :hover {
+    transform: scale(1.02);
+  }
+`
