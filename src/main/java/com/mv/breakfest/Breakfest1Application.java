435e12aaa9235b7b5f8d1c428ada0e311250bf3d
pedrovbo <pedrovicboliveira@gmail.com>
2022-04-17 21:29:10 -0300

    preparing update to origin branch

--- src/main/java/com/mv/breakfest/Breakfest1Application.java
+++ src/main/java/com/mv/breakfest/Breakfest1Application.java
@@ -2,13 +2,24 @@ package com.mv.breakfest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@Controller
+@RequestMapping("/")
 public class Breakfest1Application {
-			
+
+	
+	@GetMapping
+	public ModelAndView swaggerUi() {
+	return new ModelAndView("redirect:/swagger-ui/");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Breakfest1Application.class, args);
 	}
 
 }
-
