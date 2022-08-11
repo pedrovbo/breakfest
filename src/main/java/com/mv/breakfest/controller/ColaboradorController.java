8cfde6d07c285569a075f89fae605924ab56c5b8
prbv <prbv@gft.com>
2022-08-11 14:32:01 -0300

    change java 11-17 and clean imports

--- src/main/java/com/mv/breakfest/controller/ColaboradorController.java
+++ src/main/java/com/mv/breakfest/controller/ColaboradorController.java
@@ -1,7 +1,6 @@
 package com.mv.breakfest.controller;
 
 import java.util.List;
-import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
--- src/test/java/com/mv/breakfest/Breakfest1ApplicationTests.java
+++ src/test/java/com/mv/breakfest/Breakfest1ApplicationTests.java
@@ -3,8 +3,6 @@ package com.mv.breakfest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.mv.breakfest.entity.Colaborador;
-
 @SpringBootTest
 class Breakfest1ApplicationTests {
 
